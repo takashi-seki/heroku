93c6c23bb6e043ae406a41272568b52411f9fcfb
takashi-seki <59053145+takashi-seki@users.noreply.github.com>
2020-03-11 16:22:12 +0900

    Update NewsController.php

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,21 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illunimate\Support\	Facades\HTML;
+use Illuminate\Support\Facades\HTML;
 
+// 追記
 use App\News;
 
 class NewsController extends Controller
 {
-   public function index(Request $request)
-   {
-   	$posts=News::all()->sortByDesc('updated_at');
-   	
-   	if(count($posts)>0) {
-   		$headline = $posts->shift();
-   }  else {
-   	$healdline = null;
-   }
-   	return view('news.index', ['headline' => $headline, 'posts' => $posts]);
-   }
+    public function index(Request $request)
+    {
+        $posts = News::all()->sortByDesc('updated_at');
+
+        if (count($posts) > 0) {
+            $headline = $posts->shift();
+        } else {
+            $headline = null;
+        }
+
+        // news/index.blade.php ファイルを渡している
+        // また View テンプレートに headline、 posts、という変数を渡している
+        return view('news.index', ['headline' => $headline, 'posts' => $posts]);
+    }
 }
